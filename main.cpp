585d63100a4488af580f43f1eb313d4a5ade3906
Mikicrep <92646867+Mikicrepstudios@users.noreply.github.com>
2024-06-20 01:44:34 +0200

    Comment code for easier reading

--- main.cpp
+++ main.cpp
@@ -4,20 +4,27 @@
 #include <vector>
 
 int main(int argc, char *argv[]) {
+	// Get path argument
 	std::string directoryPath = argv[1];
+	// Vector to store files
 	std::vector<std::string> files;
 
+	// TODO: Check if theres no argument
+
+	// List files
 	for (const auto& entry : std::filesystem::directory_iterator(directoryPath)) {
 		if (entry.is_regular_file()) {
 			files.push_back(entry.path().filename().string());
 		}
 	}
 
+	// Check if directory empty
 	if (files.empty()) {
 		std::cout << "No files found in directory " << directoryPath << std::endl;
 		return 1;
 	}
 
+	// Prepare randomizer
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_int_distribution<> dist(0, files.size() - 1);
